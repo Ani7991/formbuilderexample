fa19d25d82d3bb327f83a9f322ac99c8c7d2083f
Ani7991 <an.khosteghyan@gmail.com>
2019-10-30 18:33:11 +0400

    firstcomm

---
+++ src/app/front/front.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+ import { MustMatch } from './must-mutch.validator';
+
+
+
+@Component({
+  selector: 'app-front',
+  templateUrl: './front.component.html',
+  styleUrls: ['./front.component.css']
+})
+export class FrontComponent implements OnInit {
+
+  register: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+      this.register = this.formBuilder.group({
+          username: ['', Validators.required],
+          email: ['', [Validators.required, Validators.email]],
+          password: ['', [Validators.required, Validators.minLength(8),Validators.maxLength(32)]],
+          confirmPassword: ['', Validators.required],
+         }),{
+           validator: MustMatch('password', 'confirmPassword')
+     
+    }
+  }
+    onSubmit() {
+      this.submitted = true;
+    if (this.register.invalid) {
+          return;
+      }
+
+     
+  }
+
+  onReset() {
+      this.submitted = false;
+      this.register.reset();
+  }
+  get f() { return this.register.controls; }
+}
+
